ee566166406f1e04e59fc479dad758645238bfb0
Vishal Kumar <144454292+myself-viishal@users.noreply.github.com>
2023-09-10 10:03:09 +0000

    completed

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 696969;
+let accountEmail = "20shal@gmail.com"
+var accountPassword = "12345"
+accountCity = "Daltongang" // not preffered
+
+console.table([accountId, accountEmail , accountPassword, accountCity])
+
+// Now time to change 
+// accountId = 88888888 // not possible broo
+accountEmail = '3953@gmail.com'
+accountPassword = "12@3"
+accountCity = "jamtara"
---
+++ 01_basics/02_data-types.js
@@ -0,0 +1,20 @@
+"use strict" //treat all line as newer version
+
+/*Data types*/
+
+// number => 2 to power 53
+let number = 69
+// bigint
+// string => ""
+let srtring = "Vishal Kumar"
+// boolean => true/false
+let areYouGay = true
+// null => standalone value
+let loveLife = null
+// undefined => 
+let life 
+// symbol => unique
+
+
+// null is a object
+console.log(typeof life)
