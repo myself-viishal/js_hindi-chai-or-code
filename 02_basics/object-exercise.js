0e9b25970f70b558fb98f31a115c9a930a5c8061
Vishal Kumar <144454292+myself-viishal@users.noreply.github.com>
2023-09-12 12:34:15 +0000

    done

---
+++ 02_basics/object-exercise.js
@@ -0,0 +1,15 @@
+const user = {
+    name: "Vishal",
+    lastName: 'Kumar',
+    userName: '20shal',
+}
+
+// Accessing Object Properties:
+// console.log(user.name)
+// console.log(user["name"])
+
+// Modifying Object Properties:
+user.name = 'rhaul'
+console.log(user)
+
+// Adding Object Properties:
