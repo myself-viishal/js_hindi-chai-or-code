23285b62c4d8de019becd24ab3ca04a6f6192942
Vishal Kumar <144454292+myself-viishal@users.noreply.github.com>
2023-09-21 05:15:43 +0000

    done

--- 04_controlflow/truthy.js
+++ 04_controlflow/truthy.js
@@ -6,11 +6,11 @@ if (userEmail) {
     console.log("Don't have user email");
 }
 
-// falsy values
- false, 0, -0, BigInt 0n, "", null, undefined, NaN
+//* falsy values
+//  false, 0, -0, BigInt 0n, "", null, undefined, NaN
 
-//truthy values
-"0", 'false', " ", [], {}, function(){}
+//*truthy values
+// "0", 'false', " ", [], {}, function(){}
 
 // if (userEmail.length === 0) {
 //     console.log("Array is empty");
