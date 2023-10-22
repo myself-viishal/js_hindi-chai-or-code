2be425019aedb40513bac935d104f2903a1fe15d
Vishal Kumar <144454292+myself-viishal@users.noreply.github.com>
2023-10-22 08:04:18 +0000

    completed

--- 09_Advanced_one/ApiRequestFolder/script.js
+++ 09_Advanced_one/ApiRequestFolder/script.js
@@ -5,20 +5,35 @@ let followers = document.querySelector("#followers");
 let publicRepos = document.querySelector("#repos");
 let pfp = document.querySelector(".profile-pic");
 let btn = document.querySelector("#btn");
-
-let getApi = function (repoOf) {
+let randomBtn = document.querySelector("#random-btn");
+//let getApi = function (repoOf) {
+//    let requestUrl = `https://api.github.com/users/${repoOf}`;
+//    const xhr = new XMLHttpRequest();
+//    xhr.open("GET", requestUrl);
+//    xhr.onreadystatechange = function () {
+//        console.log(xhr.readyState);
+//        if (xhr.readyState === 4) {
+//            let data = JSON.parse(xhr.responseText);
+//            console.log(data);
+//            insertingData(data);
+//        }
+//    };
+//    xhr.send();
+//};
+const randomUser = async function () {
+    let requestUrl = `https://api.github.com/users`;
+    const xhr = await fetch(requestUrl);
+    let data = await xhr.json();
+    let randoNumber = Math.floor(Math.random() * (data.length - 1 + 1) + 1);
+    console.log(data);
+    let user = data[randoNumber].login;
+    getApi(user);
+};
+const getApi = async function (repoOf) {
     let requestUrl = `https://api.github.com/users/${repoOf}`;
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", requestUrl);
-    xhr.onreadystatechange = function () {
-        console.log(xhr.readyState);
-        if (xhr.readyState === 4) {
-            let data = JSON.parse(xhr.responseText);
-            console.log(data);
-            insertingData(data);
-        }
-    };
-    xhr.send();
+    const xhr = await fetch(requestUrl);
+    let data = await xhr.json();
+    insertingData(data);
 };
 
 let insertingData = function (data) {
@@ -35,9 +50,14 @@ btn.addEventListener(
         e.preventDefault();
         let repoOfInput = document.querySelector("#repoOf");
         let repoOf = repoOfInput.value;
-        getApi(repoOf);
-        repoOfInput.value = "";
-        console.log("worked");
+        if (repoOf !== "") {
+            getApi(repoOf);
+            repoOfInput.value = "";
+            console.log("worked");
+        } else {
+            console.log("hdgjh");
+        }
     },
     false
 );
+randomBtn.addEventListener("click", randomUser);
